fff470307e6ef29b101678873ac1771f4d77f5e6
Dylan Tran <dylanbt928@gmail.com>
2024-01-30 20:55:18 -0800

    Added health and armor pickups

--- Source/RetroFPS/RetroFPSAttributeSet.cpp
+++ Source/RetroFPS/RetroFPSAttributeSet.cpp
@@ -15,10 +15,27 @@ void URetroFPSAttributeSet::PreAttributeChange(const FGameplayAttribute &Attribu
 
 bool URetroFPSAttributeSet::PreGameplayEffectExecute(FGameplayEffectModCallbackData &Data)
 {
-    float ArmorDamage = FMath::Abs(Data.EvaluatedData.Magnitude);
+    float AbsoluteMagnitude = FMath::Abs(Data.EvaluatedData.Magnitude);
 
-    if (Data.EvaluatedData.Attribute == GetArmorAttribute() && ArmorDamage > GetArmor() && Data.EvaluatedData.Magnitude < 0)
-        Health.SetCurrentValue(Health.GetCurrentValue() - (ArmorDamage - GetArmor()));
+    if (Data.EvaluatedData.Magnitude > 0)
+    {
+        if (Data.EvaluatedData.Attribute == GetArmorAttribute() && GetArmor() + AbsoluteMagnitude >= 100)
+        {
+            SetArmor(100);
+            return false;
+        }
+
+        if (Data.EvaluatedData.Attribute == GetHealthAttribute() && GetHealth() + AbsoluteMagnitude >= 100)
+        {
+            SetHealth(100);
+            return false;
+        }
+    }
+    else
+    {
+        if (Data.EvaluatedData.Attribute == GetArmorAttribute() && AbsoluteMagnitude > GetArmor())
+                Health.SetCurrentValue(Health.GetCurrentValue() - (AbsoluteMagnitude - GetArmor()));
+    }
 
     return true;
 }
